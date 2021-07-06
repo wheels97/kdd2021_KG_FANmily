12b593fe51fb7cc3a9bc53814dbbb6842347e886
wheels97 <46299461+wheels97@users.noreply.github.com>
2021-07-06 14:09:32 +0800

    Delete tt.py

--- tt.py
+++
@@ -1,7 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='manual to this script')
-parser.add_argument('--gpus', type=str, default = None)
-parser.add_argument('--batch-size', type=int, default=32)
-args = parser.parse_args()
-print(args.gpus, type(args.gpus))
-print(args.batch_size, type(args.batch_size))
